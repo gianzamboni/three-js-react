fb5ba33429186d4ac17a63ec0e6e8fdb9da4d8fd
Gianfranco Zamboni <gzamboni@dc.uba.ar>
2025-08-21 16:22:40 -0300

    Home page Drawn title

--- app/routes/home/components/demo-card/index.tsx
+++ app/routes/home/components/demo-card/index.tsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router";
 import type { Demo } from "../demo";
+import styles from "./styles.module.css";
 
 type DemoCardProps = {
   demo: Demo;
@@ -7,12 +8,12 @@ type DemoCardProps = {
 
 export const DemoCard = ({ demo }: DemoCardProps) => {
   return (
-    <Link to={demo.link} className="hand-drawn-border demo-card" >
-    <div className="border-1 rounded-md p-4 w-64 flex flex-col gap-1  transition-colors duration-300">
-      <img src={demo.thumbnail} alt={demo.title} className="w-full h-48 object-cover rounded-md box-shadow-md grayscale hover:grayscale-0 transition-all duration-300" />
-      <h2 className="text-lg font-bold mt-4">{demo.title}</h2>
-      <p className="text-sm text-gray-500 mt-2">{demo.description}</p>
-    </div>
+    <Link to={demo.link} className={styles['demo-card']}>
+      <img src={demo.thumbnail} alt={demo.title} />
+      <div>
+        <h2>{demo.title}</h2>
+        <p>{demo.description}</p>
+      </div>
     </Link>
   );
 };
