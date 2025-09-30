db79d74fa24d2a151c6b5904d52864c38f628423
Gianfranco Zamboni <gzamboni@dc.uba.ar>
2025-09-29 23:26:12 -0300

    Portal Scene clear color

--- app/routes/demos/07-portal/main-scene.tsx
+++ app/routes/demos/07-portal/main-scene.tsx
@@ -2,7 +2,7 @@ import { OrbitControls } from "@react-three/drei";
 
 export default function MainScene() {
   return <>
-    <color attach="background" args={["#000000"]} />
+    <color attach="background" args={["#080712"]} />
     <OrbitControls makeDefault />
 
     <mesh scale={1.5}>
