2af41cc7fe459d5337ad859a6a6a959a4a83ad69
Gianfranco Zamboni <gianfranco376@gmail.com>
2025-10-12 16:55:57 -0300

    Some basic events

--- app/routes/demos/08-pointer-events/main-scene.tsx
+++ app/routes/demos/08-pointer-events/main-scene.tsx
@@ -1,19 +1,29 @@
 import { OrbitControls } from "@react-three/drei";
+import { useRef } from "react";
+import type { Mesh } from "three";
 import Floor from "~/3d/floor";
 import RotatingCube from "~/3d/rotating-cube";
 import Sphere from "~/3d/sphere";
 
 export default function MainScene() {
-  return <>
 
+  const clickHandler = (event: any) => {
+    event.object.material.color.set(`hsl(${Math.random() * 360}, 100%, 75%)`);
+  }
+
+  const stopPropagation = (event: any) => {
+    event.stopPropagation();
+  }
+
+  return <>
       <OrbitControls makeDefault />
 
       <directionalLight position={ [ 1, 2, 3 ] } intensity={ 4.5 } />
       <ambientLight intensity={ 1.5 } />
 
-      <Sphere position-x={ - 2 } color="orange" />
+      <Sphere position-x={ - 2 } color="orange" onClick={stopPropagation} />
 
-      <RotatingCube position-x={ 2 } scale={ 1.5 } rotationSpeed={0.2}>
+      <RotatingCube position-x={ 2 } scale={ 1.5 } rotationSpeed={0.2} onClick={clickHandler}>
           <meshStandardMaterial color="mediumpurple" />
       </RotatingCube>
 
