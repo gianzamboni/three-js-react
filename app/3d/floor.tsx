ec237dcc8939ec2867a087d386903a6279d2c856
Gianfranco Zamboni <gianfranco376@gmail.com>
2025-10-12 16:12:28 -0300

    Floor

--- app/3d/floor.tsx
+++ app/3d/floor.tsx
@@ -3,7 +3,7 @@ import type { MeshProps } from "./types";
 
 export default function Floor({ children, ...props }: MeshProps) {
   return (
-    <mesh rotation-x={-Math.PI * 0.5} scale={10}{...props}>
+    <mesh rotation-x={-Math.PI * 0.5} scale={10} {...props}>
       <planeGeometry/>
       {children}
     </mesh>
