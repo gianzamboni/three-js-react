c3fd197f3446c92291296ddfe7694011ddc5e018
Gianfranco Zamboni <gianfranco376@gmail.com>
2025-10-12 13:33:04 -0300

    Portal shader

--- app/routes/demos/07-portal/portal.tsx
+++ app/routes/demos/07-portal/portal.tsx
@@ -1,24 +1,45 @@
 const PORTAL_URL = "/portal.glb";
 
-import { Center, useGLTF, useTexture } from "@react-three/drei";
+import { Center, useGLTF, useTexture, shaderMaterial } from "@react-three/drei";
+import { extend, useFrame } from '@react-three/fiber'
 
-import { Color, type Mesh } from "three";
+import { Color, ShaderMaterial, type Mesh } from "three";
 
 import portalVertexShader from "./shaders/portal.vert";
 import portalFragmentShader from "./shaders/portal.frag";
+import { useRef } from "react";
+
+const PortalMaterial = shaderMaterial(
+  {
+    uTime: 0,
+    uStartColor: new Color('#d8d6ff'),
+    uEndColor: new Color('#000000')
+  },
+  portalVertexShader,
+  portalFragmentShader
+)
+
+extend({ PortalMaterial });
 
 export default function Portal() {
 
   const { nodes } = useGLTF(PORTAL_URL);
   const bakedTexture = useTexture("https://i0hci4avyoqkwwp1.public.blob.vercel-storage.com/portal/portal_baked.jpg");
-  bakedTexture.flipY = false;
-
-  console.log(nodes);
+  
+  const portalMaterialRef = useRef<ShaderMaterial>(null);
 
   const bakedModel = nodes["baked"] as Mesh;
   const lampLeft = nodes["Lamp"] as Mesh;
   const lampRight = nodes["Lamp001"] as Mesh;
   const portal = nodes["Circle"] as Mesh;
+
+  bakedTexture.flipY = false;
+
+  useFrame((state, delta) => {
+    if (portalMaterialRef.current) {
+      portalMaterialRef.current.uniforms.uTime.value += delta;
+    }
+  });
 
   return <Center>
     <mesh geometry={bakedModel.geometry}>
@@ -28,15 +49,7 @@ export default function Portal() {
     </mesh>
     <mesh geometry={lampRight.geometry} />
     <mesh geometry={portal.geometry}>
-      <shaderMaterial 
-        vertexShader={portalVertexShader} 
-        fragmentShader={portalFragmentShader} 
-        uniforms={ {
-          uTime: { value: 0 },
-          uColorStart: { value: new Color('#ffffff') },
-          uColorEnd: { value: new Color('#000000') }
-        } }
-        />
+      <portalMaterial ref={portalMaterialRef} />
     </mesh>
   </Center>
 }
