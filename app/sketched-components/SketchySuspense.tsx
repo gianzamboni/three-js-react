4e136573fbabecd08c850687ce81c0e37a07a3b0
Gianfranco Zamboni <gianfranco376@gmail.com>
2025-10-27 18:43:38 -0300

    Sketchy suspense

--- app/sketched-components/SketchySuspense.tsx
+++ app/sketched-components/SketchySuspense.tsx
@@ -1,12 +1,6 @@
 import { Suspense, useEffect, useState } from "react";
 import Label from "~/sketched-components/label/label";
 
-export default function SketchySuspense({ children }: { children: React.ReactNode }) {
-  const [promise, setPromise] = useState<Promise<any> | null>(null);
-
-  useEffect(() => {
-    const promise = Promise.resolve(new Promise((resolve) => setTimeout(() => resolve(true), 10000)));
-    setPromise(promise as Promise<any>);
-  }, []);
-  return <Suspense fallback={<Label>Loading...</Label>}>{promise as React.ReactNode}</Suspense>;
+export default function SketchySuspense({ children }: { children: React.ReactNode }) {  
+  return <Suspense fallback={<Label>Loading...</Label>}>{children}</Suspense>;
 }
