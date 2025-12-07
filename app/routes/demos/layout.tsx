e58fe3abad00b360cb1a9dc44814db2b0dae6cbf
Gianfranco Zamboni <gianfranco376@gmail.com>
2025-12-07 12:55:37 -0300

    Fix: no scroll when returning home

--- app/routes/demos/layout.tsx
+++ app/routes/demos/layout.tsx
@@ -5,7 +5,7 @@ import { SketchyShadow } from "~/sketched-components/randomized-shadow";
 import SketchyLink from "~/sketched-components/link";
 
 export default function Layout() {
-  return <main className="full-screen">
+  return <main className={`full-screen ${styles['no-scroll']}`}>
     <SketchyLink to="/" className={styles['home-link']} >
         <img src="/home.svg" alt="Home Icon" className={styles['home-icon']} />
     </SketchyLink>
