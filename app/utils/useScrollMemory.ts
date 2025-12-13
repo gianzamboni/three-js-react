cee378c97e31c31966ba768a2e0118cc741ae14f
Gianfranco Zamboni <gzamboni@dc.uba.ar>
2025-12-13 17:00:48 -0300

    Home saves scroll value

---
+++ app/utils/useScrollMemory.ts
@@ -0,0 +1,32 @@
+'use client';
+
+import { useEffect, useRef } from "react";
+
+export const useScrollMemory = (storageKey: string = 'scrollTop') => {
+
+	const scrollTop = useRef(0);
+
+	useEffect(() => {
+    const savedScrollTop = localStorage?.getItem(storageKey);
+    if(savedScrollTop) {
+      scrollTop.current = parseFloat(savedScrollTop);
+    }
+
+		document.body.scrollTo({ top: scrollTop.current, behavior: 'instant' } as ScrollToOptions);
+
+		const handleBodyScroll = () => {
+			scrollTop.current = document.body.scrollTop;
+		};
+
+		document.body.addEventListener('scroll', handleBodyScroll, { passive: true });
+
+		return () => {
+			localStorage.setItem(storageKey, String(scrollTop.current));
+			document.body.removeEventListener('scroll', handleBodyScroll);
+		};
+	}, []);
+};
+
+export default useScrollMemory;
+
+
