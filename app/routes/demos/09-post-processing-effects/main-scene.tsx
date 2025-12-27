7bb6c58338741528b704b886c995e5bc3aa08a93
Gianfranco Zamboni <gianfranco376@gmail.com>
2025-12-27 20:14:51 -0300

    Effectcomposer setup

--- app/routes/demos/09-post-processing-effects/main-scene.tsx
+++ app/routes/demos/09-post-processing-effects/main-scene.tsx
@@ -2,11 +2,18 @@ import BasicSetup from "~/3d/basic-setup";
 import { PurpleCube } from "~/3d/cube";
 import { GreenFloor } from "~/3d/floor";
 import { OrangeSphere } from "~/3d/sphere";
+import { EffectComposer, ToneMapping } from '@react-three/postprocessing'
+import { ToneMappingMode } from "postprocessing";
 
 export default function MainScene() {
-    return <>
-        <BasicSetup />
 
+    console.log(ToneMappingMode)
+    return <>
+        <EffectComposer >
+            <ToneMapping mode={ ToneMappingMode.ACES_FILMIC } />
+        </EffectComposer>
+
+        <BasicSetup castShadow />
         <OrangeSphere castShadow />
         <PurpleCube castShadow />
         <GreenFloor receiveShadow />
