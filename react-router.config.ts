5c965176b472050840e833fe76b52f0546e894e8
Gianfranco Zamboni <gianfranco376@gmail.com>
2025-12-26 23:27:41 -0300

    Linter fixes

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,5 +1,6 @@
-import type { Config } from "@react-router/dev/config";
 import { vercelPreset } from '@vercel/react-router/vite';
+
+import type { Config } from "@react-router/dev/config";
 
 export default {
   // Config options...
