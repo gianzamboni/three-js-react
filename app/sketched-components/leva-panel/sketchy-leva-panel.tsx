2f15c3689a60b5977eb888086b53c07c708488a9
Gianfranco Zamboni <gzamboni@dc.uba.ar>
2026-01-04 15:48:06 -0300

    Sketchy Leva Panel correctly updating its size

--- app/sketched-components/leva-panel/sketchy-leva-panel.tsx
+++ app/sketched-components/leva-panel/sketchy-leva-panel.tsx
@@ -14,6 +14,7 @@ export default function SketchyLevaPanel({ store }: ResponsiveSketchyPanelProps)
     <SketchyBorder baseStrokeWidth='sm'>
       <MemoizedSketchyShadow strokeWidth='sm' offsetX={1} offsetY={1}/>
       <LevaPanel
+        key={store.storeId} // This is needed to force a re-render of the panel when the store changes. Otherwise, the panel will not update its size causing a visible overflow.
         store={store}
         theme={levaTheme}
         fill={true}
