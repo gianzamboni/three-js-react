b1df7fe4706784ca6fa2299af43ac78417aafd7a
Gianfranco Zamboni <gzamboni@dc.uba.ar>
2026-01-08 22:57:06 -0300

    home thumbnail and description

--- app/routes/home/demo-list.tsx
+++ app/routes/home/demo-list.tsx
@@ -22,19 +22,19 @@ const DEMOS: Demo[] = [
     id: "debug-ui",
     title: "Debug UI",
     thumbnail: "demos/debug-ui.png",
-    description: "For this demo, I added Leva and r3f-perf libraries to the project. The first one allowed me to easily add debug controls to the scene. The second one creates a monitor that shows performance metrics.",
+    description: "Added Leva and r3f-perf libraries to the project. The first one allowed me to easily add debug controls to the scene. The second one creates a monitor that shows performance metrics.",
   },
   {
     id: 'environment-staging',
     title: 'Environment and Staging',
     thumbnail: 'demos/staging.jpg',
-    description: 'This demo shows how to use environment and staging in three.js. I used the environment map from the three.js official website. I also used the staging component to create a staging area for the scene.',
+    description: 'Set up environment and staging in three.js. I used the environment map from the three.js official website. I also used the staging component to create a staging area for the scene.',
   }, 
   {
     id: 'load-models',
     title: 'Loading Models',
     thumbnail: 'demos/loaded-models.png',
-    description: "This demo shows how to load models in three.js using react three fiber and the Draco Loader. In this case, I loaded a hamburger I made using blender and a fox provided as sample by Khronos Group.",
+    description: "Loaded models in three.js using react three fiber and the Draco Loader. In this case, I used a hamburger I made using blender and a fox provided as sample by Khronos Group.",
   }, {
     id: '3d-text',
     title: '3D Text',
@@ -49,13 +49,13 @@ const DEMOS: Demo[] = [
     id: 'pointer-events',
     title: 'Pointer Events',
     thumbnail: 'demos/pointer-events.jpg',
-    description: "Some meshes that can be interacted with using pointer events. This demos uses raycasting to detect the meshes that has the cursor over them and listen for click events that affect them: When the cursor is over a clickable mesh, the cursor changes to a pointer and when the mesh is clicked, it changes color.",
+    description: "Used raycasting to detect the meshes that has the cursor over them and listen for click events that affect them: When the cursor is over a clickable mesh, the cursor changes to a pointer and when the mesh is clicked, it changes color.",
     link: "demos/pointer-events",
   }, {
     id: 'post-processing-effects',
     title: 'Post Processing Effects',
-    thumbnail: "",
-    description: "WIP",
+    thumbnail: "demos/post-processing.png",
+    description: 'A showcase of some post processing effects available in the @react-three/postprocessing library and a "Drunk" effect that I implemented from scratch.',
   }
 ].reverse();
 
