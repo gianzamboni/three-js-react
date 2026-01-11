e23912d0008acad7d0609497a4f57e3566f43711
Gianfranco Zamboni <gzamboni@dc.uba.ar>
2026-01-11 18:26:47 -0300

    Imported macbook

--- app/routes/demos/10-simple-portfolio/main-scene.tsx
+++ app/routes/demos/10-simple-portfolio/main-scene.tsx
@@ -1,12 +1,16 @@
-import { OrbitControls } from "@react-three/drei";
+import { OrbitControls, useGLTF } from "@react-three/drei";
 
 import Cube from "~/3d/cube";
 
 export default function MainScene() {
 
+  const computer = useGLTF("../models/macbook.gltf");
+  
   return <>
     <OrbitControls makeDefault />
 
+    <color args={ [ '#241a1a' ] } attach="background" />
+    <primitive object={computer.scene} />
     <Cube>
       <meshNormalMaterial />
     </Cube>
