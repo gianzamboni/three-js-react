b9bbd6f6f0de9c9084c4af47389dd7ffb63bb052
Gianfranco Zamboni <gianfranco376@gmail.com>
2026-01-15 19:20:19 -0300

    Lid open animation

--- app/routes/demos/10-simple-portfolio/computer.tsx
+++ app/routes/demos/10-simple-portfolio/computer.tsx
@@ -4,18 +4,35 @@ import { useEffect, useRef } from "react";
 
 import styles from "./styles.module.css";
 
-import type { Group } from "three";
+import { Euler, type Group } from "three";
 
 export function Computer() {
   const computer = useGLTF("../models/macbook.gltf");
   const groupRef = useRef<Group>(null);
-
+  const xRotation = useRef(Math.PI)
+  ;
   useEffect(() => {
+    const top = computer.nodes.Top;
+    const originalRotation = top.rotation.x;
+    console.log(originalRotation);
+    top.setRotationFromEuler(new Euler(xRotation.current, 0, 0));
+
     if (groupRef.current) {
       gsap.to(groupRef.current.position, {
         y: -1.2,
         duration: 5,
         ease: `elastic.out(1,0.625)`,
+      })
+        
+      gsap.to(xRotation, {
+        current: 1.3105023838474816,
+        duration: 10,
+        ease: `elastic.out(1,0.625)`,
+        delay: 1.5,
+        onUpdate: () => {
+          console.log(xRotation.current);
+          top.setRotationFromEuler(new Euler(xRotation.current, 0, 0));
+        },
       })
     }
   }, []);
