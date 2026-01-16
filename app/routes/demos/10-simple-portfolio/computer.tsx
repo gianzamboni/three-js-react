adf99445b2beadb7c5d3d5a7a94475eeb7ff7191
Gianfranco Zamboni <gzamboni@dc.uba.ar>
2026-01-15 22:48:03 -0300

    Website properly following the notebook screen

--- app/routes/demos/10-simple-portfolio/computer.tsx
+++ app/routes/demos/10-simple-portfolio/computer.tsx
@@ -1,55 +1,61 @@
 import { Html, useGLTF } from "@react-three/drei";
+import { createPortal } from "@react-three/fiber";
 import gsap from "gsap";
 import { useEffect, useRef } from "react";
+import { Euler, type Group, type Object3D } from "three";
 
 import styles from "./styles.module.css";
-
-import { Euler, type Group } from "three";
 
 export function Computer() {
   const computer = useGLTF("../models/macbook.gltf");
   const groupRef = useRef<Group>(null);
-  const xRotation = useRef(Math.PI)
-  ;
+
+  const topLid = computer.nodes.Top as Object3D;
+
+  const progress = useRef({
+    xRotation: Math.PI,
+  });
+  
   useEffect(() => {
-    const top = computer.nodes.Top;
-    const originalRotation = top.rotation.x;
+    const originalRotation = topLid.rotation.x;
     console.log(originalRotation);
-    top.setRotationFromEuler(new Euler(xRotation.current, 0, 0));
+    topLid.setRotationFromEuler(new Euler(progress.current.xRotation, 0, 0));
 
     if (groupRef.current) {
       gsap.to(groupRef.current.position, {
         y: -1.2,
         duration: 5,
         ease: `elastic.out(1,0.625)`,
-      })
-        
-      gsap.to(xRotation, {
-        current: 1.3105023838474816,
+      });
+
+      gsap.to(progress.current, {
+        xRotation: 1.3105023838474816,
         duration: 10,
         ease: `elastic.out(1,0.625)`,
         delay: 1.5,
         onUpdate: () => {
-          console.log(xRotation.current);
-          top.setRotationFromEuler(new Euler(xRotation.current, 0, 0));
+          topLid.setRotationFromEuler(new Euler(progress.current.xRotation, 0, 0));
         },
-      })
+      });
     }
   }, []);
 
   return (
     <group ref={groupRef} position-y={-10}>
-      <primitive object={computer.scene}>
+      <primitive object={computer.scene} />
+      {createPortal(
         <Html
           transform
+          occlude={'raycast'}
           wrapperClass={styles["html-screen"]}
-          distanceFactor={1.17}
-          position={[0, 1.56, -1.4]}
-          rotation-x={-0.256}
+          distanceFactor={1.95}
+          position={[0, 0, -1.925]}
+          rotation-x={-Math.PI / 2}
         >
           <iframe src="https://portfolio.gianfrancozamboni.com.ar/developer" />
-        </Html>
-      </primitive>
+        </Html>,
+        topLid
+      )}
     </group>
   );
 }
