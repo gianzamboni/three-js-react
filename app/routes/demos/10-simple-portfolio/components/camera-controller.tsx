a2fb88cdc01cad04dfa5fe06ef3c44fb317db522
Gianfranco Zamboni <gzamboni@dc.uba.ar>
2026-01-18 13:39:17 -0300

    Camera UI Controls small refactor

--- app/routes/demos/10-simple-portfolio/components/camera-controller.tsx
+++ app/routes/demos/10-simple-portfolio/components/camera-controller.tsx
@@ -1,4 +1,4 @@
-import { useFrame, useThree } from "@react-three/fiber";
+import { useFrame, useThree, type Camera } from "@react-three/fiber";
 import gsap from "gsap";
 import { useControls, levaStore } from "leva";
 import { useEffect, useRef } from "react";
@@ -22,21 +22,43 @@ const ZOOMED_IN_ROTATION: Point3D = [0, 0, 0];
 const DEFAULT_POSITION: Point3D = [-4, 3, 6];
 const DEFAULT_ROTATION: Point3D = [-0.4636476090008062, -0.5376832530932062, -0.2506869231230346];
 
+function positionControlSettings(label: 'x' | 'y' | 'z', defaultValue: number, target: Camera) {
+  return {
+    label: label,
+    value: defaultValue,
+    min: -20,
+    max: 20,
+    step: 0.01,
+    onChange: (v: number) => { target.position[label] = v; }
+  }
+};
+
+function rotationControlSettings(label: 'x' | 'y' | 'z', defaultValue: number, target: Camera) {
+  return {
+    label: label,
+    value: defaultValue,
+    min: -Math.PI,
+    max: Math.PI,
+    step: 0.01,
+    onChange: (v: number) => { target.rotation[label] = v; }
+  }
+}
+
 export function CameraController() {
   const { zoomedIn } = useSimplePortfolioState();
 
   const { camera } = useThree();
 
   const [, setPosition] = useControls('Camera Position', () => ({
-    positionX: { label: "X", value: DEFAULT_POSITION[0], min: -20, max: 20, step: 0.01, onChange: (v) => { camera.position.x = v; } },
-    positionY: { label: "Y", value: DEFAULT_POSITION[1], min: -20, max: 20, step: 0.01, onChange: (v) => { camera.position.y = v; } },
-    positionZ: { label: "Z", value: DEFAULT_POSITION[2], min: -20, max: 20, step: 0.01, onChange: (v) => { camera.position.z = v; } },
+    positionX: positionControlSettings('x', DEFAULT_POSITION[0], camera),
+    positionY: positionControlSettings('y', DEFAULT_POSITION[1], camera),
+    positionZ: positionControlSettings('z', DEFAULT_POSITION[2], camera),
   }), { store: levaStore }, [camera]);
 
   const [, setRotation] = useControls('Camera Rotation', () => ({
-    rotationX: { label: "X", value: DEFAULT_ROTATION[0], min: -Math.PI, max: Math.PI, step: 0.01, onChange: (v) => { camera.rotation.x = v; } },
-    rotationY: { label: "Y", value: DEFAULT_ROTATION[1], min: -Math.PI, max: Math.PI, step: 0.01, onChange: (v) => { camera.rotation.y = v; } },
-    rotationZ: { label: "Z", value: DEFAULT_ROTATION[2], min: -Math.PI, max: Math.PI, step: 0.01, onChange: (v) => { camera.rotation.z = v; } },
+    rotationX: rotationControlSettings('x', DEFAULT_ROTATION[0], camera),
+    rotationY: rotationControlSettings('y', DEFAULT_ROTATION[1], camera),
+    rotationZ: rotationControlSettings('z', DEFAULT_ROTATION[2], camera),
   }), { store: levaStore }, [camera]);
 
   const positionAnimationRef = useRef<gsap.core.Tween | null>(null);
@@ -55,7 +77,7 @@ export function CameraController() {
     });
   });
 
-  useEffect(() => {  
+  useEffect(() => {
     const targetPosition = zoomedIn ? ZOOMED_IN_POSITION : DEFAULT_POSITION;
     const targetRotation = zoomedIn ? ZOOMED_IN_ROTATION : DEFAULT_ROTATION;
 
