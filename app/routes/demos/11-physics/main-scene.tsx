09ac64cfb68061776fde09e405fa3d1ea6cfd3ae
Gianfranco Zamboni <gianfranco376@gmail.com>
2026-01-29 19:40:58 -0300

    Sphere and floor as rigid bodies

--- app/routes/demos/11-physics/main-scene.tsx
+++ app/routes/demos/11-physics/main-scene.tsx
@@ -1,24 +1,32 @@
-import DefaultSetup from '~/3d/basic-setup'
-import Cube from '~/3d/cube'
-import Sphere from '~/3d/sphere'
+import DefaultSetup from '~/3d/basic-setup';
+import Cube from '~/3d/cube';
+import Sphere from '~/3d/sphere';
+import { Physics, RigidBody } from '@react-three/rapier';
+import { Suspense } from 'react';
 
 export default function MainScene() {
   return <>
 
     <DefaultSetup castShadow />
+    <Suspense>
+      <Physics debug>
+        <RigidBody>
+          <Sphere castShadow position={[- 2, 2, 0]}>
+            <meshStandardMaterial color="orange" />
+          </Sphere>
+        </RigidBody>
 
-    <Sphere castShadow position={[- 2, 2, 0]}>
-      <meshStandardMaterial color="orange" />
-    </Sphere>
+        <Cube castShadow position={[2, 2, 0]}>
+          <meshStandardMaterial color="mediumpurple" />
+        </Cube>
 
-    <Cube castShadow position={[2, 2, 0]}>
-      <meshStandardMaterial color="mediumpurple" />
-    </Cube>
-
-    <mesh receiveShadow position-y={- 1.25}>
-      <boxGeometry args={[10, 0.5, 10]} />
-      <meshStandardMaterial color="greenyellow" />
-    </mesh>
-
+        <RigidBody type="fixed">
+          <mesh receiveShadow position-y={- 1.25}>
+            <boxGeometry args={[10, 0.5, 10]} />
+            <meshStandardMaterial color="greenyellow" />
+          </mesh>
+        </RigidBody>
+      </Physics>
+    </Suspense>
   </>
 }
