7c9e088b6cfee5855793376e932dbdc688f66a71
Gianfranco Zamboni <gzamboni@dc.uba.ar>
2026-01-31 21:02:58 -0300

    Twistter

--- app/routes/demos/11-physics/main-scene.tsx
+++ app/routes/demos/11-physics/main-scene.tsx
@@ -13,15 +13,36 @@ import SketchySuspense from '~/sketched-components/sketchy-suspense';
 export default function MainScene() {
 
   const cubeRef = useRef<RapierRigidBody>(null);
+  const twister = useRef<RapierRigidBody>(null);
 
   const cubeJump = () => {
-    cubeRef.current?.applyImpulse({ x: 0, y: 5, z: 0 }, true);
-    cubeRef.current?.applyTorqueImpulse({ 
-      x: gsap.utils.random(-0.5, 0.5), 
-      y: gsap.utils.random(-0.5, 0.5), 
-      z: gsap.utils.random(-0.5, 0.5) 
+    if (!cubeRef.current) return;
+    const cube = cubeRef.current;
+    const mass = cube.mass();
+    const force = 5 * mass;
+
+    cube.applyImpulse({ x: 0, y: force, z: 0 }, true);
+    cube.applyTorqueImpulse({
+      x: gsap.utils.random(-0.5, 0.5),
+      y: gsap.utils.random(-0.5, 0.5),
+      z: gsap.utils.random(-0.5, 0.5)
     }, true);
   }
+
+  useFrame((state) => {
+    if (!twister.current) return;
+
+    const time = state.clock.getElapsedTime();
+    const eulerRotation = new Euler(0, time*3, 0);
+    
+    const quaternionRotation = new Quaternion().setFromEuler(eulerRotation);
+    twister.current?.setNextKinematicRotation(quaternionRotation);
+
+    const angle = time * 0.5;
+    const x = Math.cos(angle) * 2 ;
+    const z = Math.sin(angle) * 2;
+    twister.current?.setNextKinematicTranslation({ x, y: -0.8, z });
+  })
 
   return <>
     <DefaultSetup castShadow />
