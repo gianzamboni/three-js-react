ee0ad3a05a7c6dfa9b0083e992aa9912918cd7b8
Gianfranco Zamboni <gianfranco376@gmail.com>
2026-02-26 21:53:59 -0300

    Link to work in progress (#32)
    * Link to work in progress
    
    * Update app/routes/demos/12-mini-game/interface/index.tsx
    
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- app/routes/demos/12-mini-game/interface/index.tsx
+++ app/routes/demos/12-mini-game/interface/index.tsx
@@ -59,5 +59,9 @@ export function Interface() {
           <div className={`${styles.key} ${styles.large} ${jump ? styles.active : ""}`}></div>
         </div>
     </div>
+    <div className={styles["beta-version"]}>
+      <h3>New Version</h3>
+      <p>You can try <a href="https://three-js-react-bm4577zd6-gianzambonis-projects.vercel.app/demos/mini-game" target="_blank" rel="noopener noreferrer">new beta version 2 here</a>. But beware, it's in progress, so it might not be perfect yet.</p>
+    </div>
   </div>;
 }
