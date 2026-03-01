ea220454eabbdc33e3a67fd8bba5c592fefc5f46
Gianfranco Zamboni <gzamboni@dc.uba.ar>
2026-03-01 02:15:17 -0300

    span only accepts strings

--- app/sketched-components/label/label.tsx
+++ app/sketched-components/label/label.tsx
@@ -4,17 +4,16 @@ import { Html } from "@react-three/drei";
 import styles from "./styles.module.css";
 
 import type { HtmlProps } from "@react-three/drei/web/Html";
-import type { PropsWithChildren } from "react";
 
 import { SketchyShadow } from "~/sketched-components/randomized-shadow";
 import { SketchyBorder } from "~/sketched-components/sketchy-borders";
 
-type LabelProps = Readonly<PropsWithChildren<HtmlProps>>
+type LabelProps = Readonly<HtmlProps & { children: string }>
 export default function Label({ children, ...props }: LabelProps) {
 return  <Html {...props} >
   <SketchyBorder className={styles.label} baseStrokeWidth="xs">
     <SketchyShadow strokeWidth="xs" offsetX={0.1} offsetY={2} />
     <span>{children}</span> 
   </SketchyBorder>
-</Html>;
+</Html>
 }
